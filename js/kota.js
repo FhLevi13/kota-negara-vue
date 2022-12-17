f3f77ed3e367957190d29c2f4659843868dbd6fb
FhLevi13 <alifvahlephi46@gmail.com>
2022-12-17 11:28:48 +0700

    new

--- js/kota.js
+++ js/kota.js
@@ -2,6 +2,27 @@ let vm = new Vue({
     el: "#app",
     data: {
         dataKota : null,
+
+        nama_Kota: null,
+        asal_negara: null
+    },
+    methods: {
+        simpanNegara: function() {
+            console.log("Button Disimpan");
+            let _data = {
+                'nama_Kota' : this.nama_Kota,
+                'asal_negara' : this.asal_negara
+            }
+
+            axios
+            .post('https://kota-negara-api.vercel.app/kota', _data)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
     },
     mounted() {
         axios
--- js/negara.js
+++ js/negara.js
@@ -2,6 +2,27 @@ let vm = new Vue({
     el: "#app",
     data: {
         dataNegara : null,
+
+        nama_negara: null,
+        ibukota: null
+    },
+    methods: {
+        simpanNegara: function() {
+            console.log("Button Disimpan");
+            let _data = {
+                'nama_negara' : this.nama_negara,
+                'ibukota' : this.ibukota
+            }
+
+            axios
+            .post('https://kota-negara-api.vercel.app/negara', _data)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
     },
     mounted() {
         axios
